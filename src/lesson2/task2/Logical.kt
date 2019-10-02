57775e1b06f2d6d280e0a7a1583ae346016c5fef
vladilenababina <vladilena_babina_01@icloud.com>
2019-10-02 22:58:56 +0300

    Решены все задачи

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -10,7 +10,7 @@ import kotlin.math.abs
  *
  * Лежит ли точка (x, y) внутри окружности с центром в (x0, y0) и радиусом r?
  */
-fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
+fun pointInsideCircle (x: Double, y: Double, x0: Double, y0: Double, r: Double) =
     sqr(x - x0) + sqr(y - y0) <= sqr(r)
 
 /**
